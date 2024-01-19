fb0280aba29966d3eee997f58426bcb4514f9e62
amitkumarjhookiya@gmail.com <amitkumarjhookiya@gmail.com>
2024-01-19 15:48:26 +0530

    fix_problam

--- index.php
+++ index.php
@@ -139,11 +139,9 @@
 
 <div id="body1"></div>
 
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="./jquery.js"></script>
         <script src="main.js"></script>
        <script src="_.js"></script>
 
 </body>
-
 </html>
--- php/conn.php
+++ php/conn.php
@@ -1,20 +1,20 @@
 <?php
 
 
-
+/*
             $servername = "sql309.infinityfree.com";
              $username = "if0_35321460";
              $password = "i93QJulP3Hbl";
          $dbname = "if0_35321460_SarkarR"; // Your database name
 
+*/
 
-
- // $servername = "127.0.0.1";
-  // $username = "root";
- //$password = "root";
- // $dbname = "SarkarR";
-
+ $servername = "127.0.0.1";
+  $username = "root";
+ $password = "root";
+  $dbname = "SarkarR";
 // Create a database connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
